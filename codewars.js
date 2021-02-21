0878446c9e252804f310fe5f84dda2faf67b3d72
CodeMahmoud <43242909+CodeMahmoud@users.noreply.github.com>
2021-02-20 19:08:09 -0600

    Create codewars.js

---
+++ codewars.js
@@ -0,0 +1,29 @@
+1.Remove First and Last Character
+function removeChar(str){
+ //You got this!
+return str.slice(1,-1);
+};
+
+2.Remove String Spaces
+function noSpace(x){
+ return x.replace(/\s/g, '');
+
+}
+
+
+3.Return Negative
+function makeNegative(num) {
+  // Code?
+  if (num > 0){
+    return -num
+  }else{
+    return num
+    
+  }
+  
+}
+4.Opposite number
+function opposite(number) {
+  //your code here
+  return -number
+}
